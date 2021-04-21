29e5af31f33c679d118c8bf30b6742b25fe7c6fc
Tejas Guduru <tejas.guduru@outlook.com>
2021-04-21 22:34:28 +0530

    signin and signup works!

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import "../styles.css";
 
 export default function Home() {
     console.log("API IS", API);
     return (
-        <Base title="Home page"> 
+        <Base title="Home page" description="Welcome to the tshirt store"> 
             <div className="row">
                 <div className="col-4">
                     <button className="btn btn-success">TEST</button>
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,47 +1,53 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom';
 
-const Menu=()=> (
-    <div>
-        <ul className="nav nav-tabs bg-dark">
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                    Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                    Cart
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                   Dashboard
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                    A.Dashboard
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                   SignUp
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                    SignIn
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                    SignOut
-                </Link>
-            </li>
-        </ul>
-     </div>
-        
-    )
+const currentTab = (history, path) => {
+    if (history.location.pathname === path)
+        return { color: "#4DD637" }
+    else
+        return { color: "#FFFFFF" }
+}
+
+const Menu = ({history}) => (
+  <div>
+    <ul className="nav nav-tabs bg-dark">
+      <li className="nav-item">
+        <Link style={currentTab(history, "/")} className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">
+          Cart
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">
+          Dashboard
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">
+          A.Dashboard
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link style={currentTab(history, "/user/signup")} className="nav-link" to="/signup">
+          SignUp
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link style={currentTab(history, "/user/signin")} className="nav-link" to="/signin">
+          SignIn
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link style={currentTab(history, "/user/signout")}  className="nav-link" to="/signout">
+          SignOut
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
 
 export default withRouter(Menu);
