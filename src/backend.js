f56b120b1fa6f502c162fe7d2fc35bfa8404fc36
Tejas Guduru <tejas.guduru@outlook.com>
2021-06-12 11:36:47 +0530

    cors-everywhere proxy test

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,3 @@
 //export const API = process.env.REACT_APP_BACKEND;
 export const API =
-  "http://ecommbackend-env.eba-bc3kksqd.us-east-2.elasticbeanstalk.com/api/";
+  "https://cors-everywhere.herokuapp.com/http://myapi.com/v1/users/http://ecommbackend-env.eba-bc3kksqd.us-east-2.elasticbeanstalk.com/api/";
