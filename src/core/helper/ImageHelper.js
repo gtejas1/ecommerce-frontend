1148ac56954a384864b9107d9e73ca3d958ba4f2
Tejas Guduru <tejas.guduru@outlook.com>
2021-06-30 23:25:58 +0530

    s3 image link updated

--- src/core/helper/ImageHelper.js
+++ src/core/helper/ImageHelper.js
@@ -6,7 +6,7 @@ const ImageHelper = ({ product }) => {
   //   ? `${API}/product/photo/${product._id}`
   //   : `https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
   const imageUrl =
-    "https://ecomm-bckend-tshirts.s3.ap-south-1.amazonaws.com/c853fa8f-34be-4511-8b23-f05524ee4ff7.jpeg";
+    "https://ecomm-bckend-tshirts.s3.ap-south-1.amazonaws.com/seven.jpg";
   return (
     <div className="rounded border border-success p-2">
       <img
