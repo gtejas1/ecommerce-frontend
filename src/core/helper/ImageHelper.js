7fc1c1484c1b5f04b3c8d97f4cef98dc40c1f815
Tejas Guduru <tejas.guduru@outlook.com>
2021-07-01 02:31:54 +0530

    S3 image src test

--- src/core/helper/ImageHelper.js
+++ src/core/helper/ImageHelper.js
@@ -2,11 +2,10 @@ import React from "react";
 import { API } from "../../backend";
 
 const ImageHelper = ({ product }) => {
-  // const imageUrl = product
-  //   ? `${API}/product/photo/${product._id}`
-  //   : `https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
-  const imageUrl =
-    "https://ecomm-bckend-tshirts.s3.ap-south-1.amazonaws.com/seven.jpg";
+  const imageUrl = product
+    ? `${product.photo}`
+    : `https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
+
   return (
     <div className="rounded border border-success p-2">
       <img
