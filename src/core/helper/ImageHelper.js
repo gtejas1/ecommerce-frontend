28a7bc505472bd57d32df97a79fe8304d64dc887
Tejas Guduru <tejas.guduru@outlook.com>
2021-06-30 23:11:38 +0530

    gdrive image src

--- src/core/helper/ImageHelper.js
+++ src/core/helper/ImageHelper.js
@@ -2,9 +2,11 @@ import React from "react";
 import { API } from "../../backend";
 
 const ImageHelper = ({ product }) => {
-  const imageUrl = product
-    ? `${API}/product/photo/${product._id}`
-    : `https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
+  // const imageUrl = product
+  //   ? `${API}/product/photo/${product._id}`
+  //   : `https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
+  const imageUrl =
+    "https://drive.google.com/file/d/1yGuajEhDY7Ofy8apq3cb-EaJzfdoTn6E/view?usp=sharing";
   return (
     <div className="rounded border border-success p-2">
       <img
